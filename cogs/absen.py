e701d21f8b073a02f179b1bd7ef2be9bfd9b8136
Visqy <62652907+Visqy@users.noreply.github.com>
2021-07-27 11:56:10 +0700

    changing entry

--- cogs/absen.py
+++ cogs/absen.py
@@ -22,7 +22,7 @@ async def script(content):
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
     date= datetime.datetime.strptime(content[2], '%m/%d/%y %H:%M:%S')
     days={'Mon':'Senin', 'Tue':'Selasa', 'Wed':'Rabu', 'Thu':'Kamis', 'Fri':'Jumat', 'Sat':'Sabtu'}
-    driver.get(os.getenv("LINK_GFORM").format(content[1].replace(' ','+'),'915106473' if content[1]=='XII IPA 1' else '670905647', content[0].replace(' ','+').replace('^', "'"), content[3], date.strftime('%Y-%m-%d'), days[date.strftime('%a')], date.strftime('%H:%M')))
+    driver.get(os.getenv("LINK_GFORM").format(content[1].replace(' ','+'),'1143179380' if content[1]=='XII IPA 1' else '1955799690', content[0].replace(' ','+').replace('^', "'"), content[3], date.strftime('%Y-%m-%d'), days[date.strftime('%a')], date.strftime('%H:%M')))
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div"))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
