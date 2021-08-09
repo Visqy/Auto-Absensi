b567fabb2187ac1fb7fc9b22aa2620d986704cf6
Visqy <newunban@gmail.com>
2021-08-09 15:27:44 +0700

    changing link

--- cogs/absen.py
+++ cogs/absen.py
@@ -21,13 +21,14 @@ async def script(content):
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
     date= datetime.datetime.strptime(content[2], '%m/%d/%y %H:%M:%S')
-    days={'Mon':'Senin', 'Tue':'Selasa', 'Wed':'Rabu', 'Thu':'Kamis', 'Fri':'Jumat', 'Sat':'Sabtu'}
+    days={'Mon':'Senin', 'Tue':'selasa', 'Wed':'Rabu', 'Thu':'Kamis', 'Fri':'Jumat', 'Sat':'Sabtu'}
     try:
-        driver.get(os.getenv("LINK_GFORM").format(content[1].replace(' ','+'),'1143179380' if content[1]=='XII IPA 1' else '1955799690', content[0].replace(' ','+').replace('^', "'"), content[3], date.strftime('%Y-%m-%d'), days[date.strftime('%a')], date.strftime('%H:%M')))
+        #driver.get(os.getenv("LINK_GFORM").format(content[1].replace(' ','+'),'1143179380' if content[1]=='XII IPA 1' else '1955799690', content[0].replace(' ','+').replace('^', "'"), content[3], date.strftime('%Y-%m-%d'), days[date.strftime('%a')], date.strftime('%H:%M')))
+        driver.get(os.getenv("LINK_GFORM").format(content[1].replace(' ','+'), content[0].replace(' ','+').replace('^', "'"), content[3], date.strftime('%Y-%m-%d'), days[date.strftime('%a')]))
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
+        # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
+        # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
+        # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div/div[2]"))).click()
         WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/div[3]")))
     finally:
         driver.quit()
@@ -38,8 +39,8 @@ class Absen(commands.Cog):
 
     @commands.command()
     @commands.cooldown(rate=1, per=3)
-    async def absen(self, ctx, p='datang'):
-        prep = ['datang','pulang']
+    async def absen(self, ctx, p=''):
+        prep = ['pembukaan','penutupan']
         if p in prep:
             author = ctx.message.author
             cur = self.bot.db.cursor()
